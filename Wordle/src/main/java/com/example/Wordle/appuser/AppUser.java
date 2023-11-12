13e6fb6b73a98e9db1249c547282254bfb82eb5d
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-11-12 20:25:57 +0100

    Repository and service created

--- Wordle/src/main/java/com/example/Wordle/appuser/AppUser.java
+++ Wordle/src/main/java/com/example/Wordle/appuser/AppUser.java
@@ -1,5 +1,10 @@
 package com.example.Wordle.appuser;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,17 +12,45 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 public class AppUser implements UserDetails {
 
+    @SequenceGenerator(
+            name = "app_user_seq",
+            sequenceName = "app_user_seq",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "app_user_seq"
+    )
     private Long id;
     private String username;
     private String password;
     private String name;
     private String surname;
     private String email;
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
+    public AppUser(String username, String password, String name, String surname, String email, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
