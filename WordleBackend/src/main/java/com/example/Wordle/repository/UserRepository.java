b022225a85b866024117d34e465b30b1ee2ac1cd
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-11-23 23:35:52 +0100

    Revert "Merge pull request #3 from potrec/Subtask-1-3"
    This reverts commit bbe33f46ca2f20ad37e4be06ad39f380bcdb9668, reversing
    changes made to 40ce6a0462fb1ba821f31ee96ae5a1b672fa30dc.

--- WordleBackend/src/main/java/com/example/Wordle/repository/UserRepository.java
+++ WordleBackend/src/main/java/com/example/Wordle/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.example.Wordle.repository;
 import com.example.Wordle.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
