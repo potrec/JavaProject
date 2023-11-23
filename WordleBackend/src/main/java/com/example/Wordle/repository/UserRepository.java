cb60fb5ef99adbb4126abca24466dd10c7fcd447
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2023-11-23 20:13:34 +0100

    Another dose of tests

--- WordleBackend/src/main/java/com/example/Wordle/repository/UserRepository.java
+++ WordleBackend/src/main/java/com/example/Wordle/repository/UserRepository.java
@@ -3,9 +3,8 @@ package com.example.Wordle.repository;
 import com.example.Wordle.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
 }
--- WordleBackend/src/test/java/com/example/Wordle/repository/UserRepositoryTest.java
+++ WordleBackend/src/test/java/com/example/Wordle/repository/UserRepositoryTest.java
@@ -2,20 +2,32 @@ package com.example.Wordle.repository;
 
 import com.example.Wordle.enums.AppUserRole;
 import com.example.Wordle.models.User;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @DataJpaTest
+@SpringBootTest
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
     @Test
     public void UserRepositorySaveUser() {
-        User user = User.builder().username("Username").password("Password").email("email@example.com").appUserRole(AppUserRole.USER).build();
+        Assertions.assertThat(userRepository).isNotNull();
+
+        User user = User.builder()
+                .username("Username")
+                .password("Password")
+                .email("email@example.com")
+                .appUserRole(AppUserRole.USER).build();
+        Assertions.assertThat(user).isNotNull();
 
         User savedUser = userRepository.save(user);
 
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
     }
 }
